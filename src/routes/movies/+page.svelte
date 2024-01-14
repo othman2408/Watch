57f2c1015025d67c5d4c541cc4789c2b8c38e5d5
Othman Alibrahim <acerxr@gmail.com>
2024-01-15 02:19:58 +0300

    card

--- src/routes/movies/+page.svelte
+++ src/routes/movies/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+	import Cardo from './../../lib/my-component/Cardo.svelte';
 	import Card from '../../lib/my-component/Card.svelte';
 
 	export let data;
@@ -7,13 +8,9 @@
 
 <main>
 	<h1 class="pb-5 text-center text-2xl font-bold">Movies</h1>
-	<div class="content grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3">
+	<div class="content grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3">
 		{#each data.movies as movie}
-			<Card
-				title={movie.title}
-				desc={movie.overview}
-				src={'https://images.unsplash.com/photo-1703700414418-2969a8dc8978?q=80&w=1931&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'}
-			/>
+			<Cardo />
 		{/each}
 	</div>
 </main>
