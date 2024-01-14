b9cdfe4f38f1cc5e2de74487d37cbc02a984e98c
Othman <acerxr@gmail.com>
2024-01-14 11:43:13 +0300

    API

---
+++ src/lib/my-component/Card.svelte
@@ -0,0 +1,14 @@
+<script>
+	import * as Card from '$lib/components/ui/card';
+
+	export let title, desc, src;
+</script>
+
+<Card.Root>
+	<Card.Header>
+		<img class="h-auto max-h-60 w-full object-cover pb-2" {src} />
+		<Card.Title>{title}</Card.Title>
+		<Card.Description>{desc}</Card.Description>
+	</Card.Header>
+	<Card.Content></Card.Content>
+</Card.Root>
